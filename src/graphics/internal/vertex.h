7d34b296e30bb1d92e18687bd88fff363c668da2
JaviCervera <JaviCervera@users.noreply.github.com>
2023-05-26 21:21:25 +0200

    Modernized Vertex

--- src/graphics/internal/vertex.h
+++ src/graphics/internal/vertex.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
 #include "../../math/vec3.h"
 #include "../../real.h"
@@ -7,7 +8,7 @@
 struct Vertex {
 	Vec3r m_position;
 	uint32_t m_color;
-	real_t m_texCoords[2];
+	std::array<real_t, 2> m_texCoords;
 
 	constexpr Vertex(const Vec3r& position, uint32_t color, real_t u, real_t v) noexcept
 	: m_position{position}, m_color{color}, m_texCoords{u, v} {}
