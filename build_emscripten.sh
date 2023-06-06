bacd2de3035d81692aeeab7891e5f6c816a980cd
JaviCervera <JaviCervera@users.noreply.github.com>
2023-06-06 21:15:38 +0200

    Emscripten build action now stops if compilation fails

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -9,6 +9,9 @@ echo "# Building (Emscripten) ..."
 emcmake cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -B _CMAKE/_EMSCRIPTEN
 cd _CMAKE/_EMSCRIPTEN
 emmake make NDEBUG=1
+if [ $? -ne 0 ]; then
+  exit 1
+fi
 mv hax.js ../../_build/hax.js
 mv hax.wasm ../../_build/hax.wasm
 mv hax.data ../../_build/hax.data
